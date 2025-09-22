73cda34d1a8e833605e7b48a91a3efd7a00e0cf6
AmirShamim <amirshamim412@gmail.com>
2025-09-22 15:07:43 +0530

    Deployed to vercel and added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Home from './pages/Home';
 import './index.css';
 
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  ) ;
 }
 
 export default App;
